8f5db3726ad4cc519cd742f6ff5b0f1b89e1e166
Ryan Moran <ryan@rtmoran.org>
2019-04-30 23:45:20 -0400

    improved enc/dec/brute functions
    Modified encryption, decryption, and brute force functions. Shift of any magnitude now works with revised modulus call.  Bruteforce function modified to provide instructor's expected results.

--- Menu.cpp
+++ Menu.cpp
@@ -3,9 +3,8 @@
 //defining the constructor
 Menu::Menu() {}
 
-void Menu::mainMenu()
+void Menu::mainMenu(int choice1)
 {
-	int choice1;
 	cout << endl;
 	cout << "************************************************" << endl;
 	cout << "* Welcome to the Encryption/Decryption Machine *" << endl;
@@ -19,21 +18,9 @@ void Menu::mainMenu()
 	cout << "3-> Exit" << endl;
 	cin >> choice1;
 	mainChoice = choice1;
-	switch (mainChoice)
-	{
-	case 1:
-		encryptMenu();
-		break;
-	case 2:
-		decryptMenu();
-		break;
-	default:
-		return;
-	}
 }
-void Menu::encryptMenu()
+void Menu::encryptMenu(int choice2)
 {
-	int choice2;
 	cout << endl;
 	cout << "**********************************************************" << endl;
 	cout << "*                      Encryption                        *" << endl;
@@ -47,22 +34,9 @@ void Menu::encryptMenu()
 	cout << "3-> Return to Main Menu" << endl;
 	cin >> choice2;
 	encryptChoice = choice2;
-	switch (encryptChoice)
-	{
-	case 1:
-		enSubMenu();
-		break;
-	case 2:
-		enCaeMenu();
-		break;
-	default:
-		mainMenu();
-		break;
-	}
 }
-void Menu::decryptMenu()
+void Menu::decryptMenu(int choice3)
 {
-	int choice3;
 	cout << endl;
 	cout << "**********************************************************" << endl;
 	cout << "*                      Decryption                        *" << endl;
@@ -77,48 +51,35 @@ void Menu::decryptMenu()
 	cout << "4-> Return to Main Menu" << endl;
 	cin >> choice3;
 	decryptChoice = choice3;
-	switch (decryptChoice)
-	{
-	case 1:
-		deSubMenu();
-		break;
-	case 2:
-		deCaeMenu();
-		break;
-	default:
-		bfMenu();
-		break;
-	}
 }
-string Menu::subEncrypt(string cleartext, int shift = 0) {
+string Menu::subEncrypt(string cleartext) {
 
 	string alpha = "kfleyinjxrawdchvumtqzpsgob";
 	string subalpha = "moswlfxtecbjnypzkgqudhvria";
 	string ciphertext;
 	int subindex;
 
-	for (int i{ 0 }; i <= cleartext.length(); i++) {
+	for (int i{ 0 }; i < cleartext.length(); i++) {
 		for (int j{ 0 }; j < 26; j++) {
 			if (cleartext[i] == ' ') {
 				ciphertext += ' ';
 				break;
 			}
 			if (cleartext[i] == alpha[j]) {
-				subindex = (j + shift) % 26;
-				ciphertext += subalpha[subindex];
+				ciphertext += subalpha[j];
 				break;
 			}
 		}
 	}
 	return ciphertext;
 }
-string Menu::caesarEncrypt(string cleartext, int shift = 1) {
+string Menu::caesarEncrypt(string cleartext, int shift = 3) {
 
 	string alpha = "abcdefghijklmnopqrstuvwxyz";
 	string ciphertext;
 	int caesarindex;
 
-	for (int i{ 0 }; i <= cleartext.length(); i++) {
+	for (int i{ 0 }; i < cleartext.length(); i++) {
 		for (int j{ 0 }; j < 26; j++) {
 			if (cleartext[i] == ' ') {
 				ciphertext += ' ';
@@ -133,9 +94,8 @@ string Menu::caesarEncrypt(string cleartext, int shift = 1) {
 	}
 	return ciphertext;
 }
-void Menu::enSubMenu()
+void Menu::enSubMenu(int choice4)
 {
-	int choice4;
 	cout << endl;
 	cout << "*************************************" << endl;
 	cout << "*            Encryption             *" << endl;
@@ -149,25 +109,9 @@ void Menu::enSubMenu()
 	cout << "2-> Return to the Encryption Menu" << endl;
 	cin >> choice4;
 	enSubChoice = choice4;
-	if (enSubChoice == 1)
-	{
-		string cleartext;
-		cout << "-> ";
-		getline(cin, cleartext);
-		subEncrypt(cleartext);
-
-		cout << endl;
-
-		superMenu();
-	}
-	else
-	{
-		encryptMenu();
-	}
 }
-void Menu::enCaeMenu()
+void Menu::enCaeMenu(int choice5)
 {
-	int choice5;
 	cout << endl;
 	cout << "*************************************" << endl;
 	cout << "*            Encryption             *" << endl;
@@ -181,38 +125,22 @@ void Menu::enCaeMenu()
 	cout << "2-> Return to the Encryption Menu" << endl;
 	cin >> choice5;
 	enCaeChoice = choice5;
-	if (enCaeChoice == 1)
-	{
-		string cleartext;
-		cout << "-> ";
-		getline(cin, cleartext);
-		caesarEncrypt(cleartext);
-
-		cout << endl;
-
-		superMenu();
-	}
-	else
-	{
-		encryptMenu();
-	}
 }
-string Menu::subDecrypt(string cleartext, int shift = 0) {
+string Menu::subDecrypt(string cleartext) {
 
 	string alpha = "kfleyinjxrawdchvumtqzpsgob";
 	string subalpha = "moswlfxtecbjnypzkgqudhvria";
 	string ciphertext;
 	int subindex;
 
-	for (int i{ 0 }; i <= cleartext.length(); i++) {
+	for (int i{ 0 }; i < cleartext.length(); i++) {
 		for (int j{ 0 }; j < 26; j++) {
 			if (cleartext[i] == ' ') {
 				ciphertext += ' ';
 				break;
 			}
 			if (cleartext[i] == subalpha[j]) {
-				subindex = (j + shift) % 26;
-				ciphertext += alpha[subindex];
+				ciphertext += alpha[j];
 				break;
 			}
 		}
@@ -225,14 +153,14 @@ string Menu::caesarDecrypt(string cleartext, int shift = 1) {
 	string ciphertext;
 	int caesarindex;
 
-	for (int i{ 0 }; i <= cleartext.length(); i++) {
+	for (int i{ 0 }; i < cleartext.length(); i++) {
 		for (int j{ 0 }; j < 26; j++) {
 			if (cleartext[i] == ' ') {
 				ciphertext += ' ';
 				break;
 			}
 			if (cleartext[i] == alpha[j]) {
-				caesarindex = (j - shift) % 26;
+				caesarindex = (26 + j - shift) % 26;
 				ciphertext += alpha[caesarindex];
 				break;
 			}
@@ -242,15 +170,14 @@ string Menu::caesarDecrypt(string cleartext, int shift = 1) {
 }
 void Menu::bruteforce(string ciphertext) {
 
-	cout << left << setw(35) << "Caesar Brute Force" << "Substitution Brute Force" << endl;
-	for (int shift{ 1 }; shift <= 26; shift++) {
-		cout << left << setw(35) << caesarEncrypt(ciphertext, shift);
-		cout << subDecrypt(ciphertext, shift) << endl;
-	}
+    cout << left << setw(35) << "Caesar Brute Force" << "Substitution Brute Force"<< endl;
+    cout << setw(35) << "" << subDecrypt(ciphertext) << endl;
+    for(int shift{1};shift<=26;shift++) {
+        cout << left << setw(35) << caesarEncrypt(ciphertext,shift) << endl;
+    }
 }
-void Menu::deSubMenu()
+void Menu::deSubMenu(int choice6)
 {
-	int choice6;
 	cout << endl;
 	cout << "*************************************" << endl;
 	cout << "*            Decryption             *" << endl;
@@ -264,25 +191,9 @@ void Menu::deSubMenu()
 	cout << "2-> Return to the Decryption Menu" << endl;
 	cin >> choice6;
 	deSubChoice = choice6;
-	if (deSubChoice == 1)
-	{
-		string cleartext;
-		cout << "-> ";
-		getline(cin, cleartext);
-		subDecrypt(cleartext);
-
-		cout << endl;
-
-		superMenu();
-	}
-	else
-	{
-		decryptMenu();
-	}
 }
-void Menu::deCaeMenu()
+void Menu::deCaeMenu(int choice7)
 {
-	int choice7;
 	cout << endl;
 	cout << "*************************************" << endl;
 	cout << "*            Decryption             *" << endl;
@@ -296,25 +207,9 @@ void Menu::deCaeMenu()
 	cout << "2-> Return to the decryption Menu" << endl;
 	cin >> choice7;
 	deCaeChoice = choice7;
-	if (deCaeChoice == 1)
-	{
-		string cleartext;
-		cout << "-> ";
-		getline(cin, cleartext);
-		caesarDecrypt(cleartext);
-
-		cout << endl;
-
-		superMenu();
-	}
-	else
-	{
-		decryptMenu();
-	}
 }
-void Menu::bfMenu()
+void Menu::bfMenu(int choice8)
 {
-	int choice8;
 	cout << endl;
 	cout << "*************************************" << endl;
 	cout << "*            Decryption             *" << endl;
@@ -328,49 +223,4 @@ void Menu::bfMenu()
 	cout << "2-> Return to the decryption Menu" << endl;
 	cin >> choice8;
 	bfChoice = choice8;
-	if (bfChoice == 1)
-	{
-		string cleartext;
-		cout << "-> ";
-		getline(cin, cleartext);
-		bruteforce(cleartext);
-
-		cout << endl;
-
-		superMenu();
-
-	}
-	else
-	{
-		decryptMenu();
-	}
-}
-void Menu::superMenu()
-{
-	int choice9;
-	cout << endl;
-	cout << "***************************" << endl;
-	cout << "*   Please choose where   *" << endl;
-	cout << "* you'd like to return to *" << endl;
-	cout << "*   from the menu below   *" << endl;
-	cout << "***************************" << endl;
-	cout << endl;
-
-	cout << "1-> Encryption Menu" << endl;
-	cout << "2-> Decryption Menu" << endl;
-	cout << "3-> Main Menu" << endl;
-	cin >> choice9;
-	superChoice = choice9;
-	switch (superChoice)
-	{
-	case 1:
-		encryptMenu();
-		break;
-	case 2:
-		decryptMenu();
-		break;
-	default:
-		mainMenu();
-		break;
-	}
 }
